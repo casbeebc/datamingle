253a54008faa0cf042da1dc05c310d40689946f2
Brett Casbeer <casbeebc@gmail.com>
2013-09-14 16:44:19 -0700

    Added Google Analytics

--- wp-content/themes/bootstrap/header.php
+++ wp-content/themes/bootstrap/header.php
@@ -35,7 +35,22 @@
 
 		<!-- typeahead plugin - if top nav search bar enabled -->
 		<?php require_once('library/typeahead.php'); ?>
-				
+		
+		<!-- Google Analytics -->
+		<script type="text/javascript">
+
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-1944083-9']);
+          _gaq.push(['_setDomainName', 'datamingle.com']);
+          _gaq.push(['_trackPageview']);
+        
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        
+        </script>		
 	</head>
 	
 	<body <?php body_class(); ?> style="margin: 0 0 0 0; padding: 0 0 0 0;">		
